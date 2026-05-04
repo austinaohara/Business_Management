022e64add439633e30f7462912c263c1c5519492
Giovanni6722 <giodude.dev@gmail.com>
2026-05-04 18:37:48 -0400

    Began registration implementation, notes below:
    - In login.fxml: Added sign up button and text
    - In main.css: Added .authlink
    - Created registration.fxml - new page for registration
    - Created ResistrationController.java - Stub class with @FXML references

---
+++ src/main/java/edu/farmingdale/RegistrationController.java
@@ -0,0 +1,16 @@
+package edu.farmingdale;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+public class RegistrationController {
+
+    @FXML private BorderPane rootPane;
+    @FXML private TextField usernameField;
+    @FXML private PasswordField passwordField;
+    @FXML private PasswordField confirmPasswordField;
+    @FXML private Label statusLabel;
+}
